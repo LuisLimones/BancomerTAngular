3be11745284b13229d5ea8048899a5e1f9883353
Oscar Enrique Escobar de la Cruz <ozkarescorpion@gmail.com>
2019-08-19 19:59:05 -0500

    stylo login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './componentes/login/login.component';
 
 
 
 const routes: Routes = [
-  {path: '**', redirectTo: "" }
+  {path: '', component: LoginComponent },
+  {path: 'login', component:LoginComponent},
  
 ];
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
